b45b80f43ef0033b18c513183c812b388229085f
JSH <215212g@gmail.com>
2023-09-13 10:13:13 +0900

    [common][add]프로젝트 초기셋팅

---
+++ src/main/java/kr/co/fastcampus/board/SpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProjectApplication.class, args);
+    }
+
+}
