fedbe9c50f7323920e1ee9f7a33a74f135f20b97
JSH <215212g@gmail.com>
2023-09-13 21:21:56 +0900

    [board][add] index페이지 추가

--- src/main/java/kr/co/fastcampus/board/controller/ArticleController.java
+++ src/main/java/kr/co/fastcampus/board/controller/ArticleController.java
@@ -1,10 +1,18 @@
 package kr.co.fastcampus.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
-
+    @GetMapping
+    public String articles(ModelMap modelMap){
+        modelMap.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
