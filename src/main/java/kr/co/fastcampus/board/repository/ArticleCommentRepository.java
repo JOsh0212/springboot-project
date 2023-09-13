f3a9d821f76cdf583fc5bee3d657dd852f955a1b
JSH <215212g@gmail.com>
2023-09-13 15:39:40 +0900

    [board][add] rest, hal 추가

--- src/main/java/kr/co/fastcampus/board/repository/ArticleCommentRepository.java
+++ src/main/java/kr/co/fastcampus/board/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package kr.co.fastcampus.board.repository;
 
 import kr.co.fastcampus.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
--- src/main/java/kr/co/fastcampus/board/repository/ArticleRepository.java
+++ src/main/java/kr/co/fastcampus/board/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package kr.co.fastcampus.board.repository;
 
 import kr.co.fastcampus.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
