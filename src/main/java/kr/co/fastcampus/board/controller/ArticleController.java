f1ac709db72097cb52c91053745035c7a093fc82
JSH <215212g@gmail.com>
2023-09-15 09:31:01 +0900

    [board][add] 페이지 네이션 기능 구현

--- src/main/java/kr/co/fastcampus/board/controller/ArticleController.java
+++ src/main/java/kr/co/fastcampus/board/controller/ArticleController.java
@@ -4,7 +4,9 @@ import kr.co.fastcampus.board.domain.type.SearchType;
 import kr.co.fastcampus.board.dto.response.ArticleResponse;
 import kr.co.fastcampus.board.dto.response.ArticleWithCommentResponse;
 import kr.co.fastcampus.board.service.ArticleService;
+import kr.co.fastcampus.board.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,19 +17,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/articles")
 @RequiredArgsConstructor
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size=10,sort = "createdAt",direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap modelMap){
-        modelMap.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(),articles.getTotalPages());
+        modelMap.addAttribute("articles", articles);
+        modelMap.addAttribute("paginationBarNumber", barNumbers);
         return "articles/index";
     }
     @GetMapping("/{articleId}")
