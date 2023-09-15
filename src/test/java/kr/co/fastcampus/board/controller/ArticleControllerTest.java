7ed0b426ec9c8804d65909611feaf6366dc8a501
JSH <215212g@gmail.com>
2023-09-15 11:07:24 +0900

    [board][fix] 게시판 검색 구현

--- src/test/java/kr/co/fastcampus/board/controller/ArticleControllerTest.java
+++ src/test/java/kr/co/fastcampus/board/controller/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package kr.co.fastcampus.board.controller;
 
 import kr.co.fastcampus.board.config.SecurityConfig;
+import kr.co.fastcampus.board.domain.type.SearchType;
 import kr.co.fastcampus.board.dto.ArticleWithCommentsDTO;
 import kr.co.fastcampus.board.dto.UserAccountDTO;
 import kr.co.fastcampus.board.service.ArticleService;
@@ -59,6 +60,30 @@ class ArticleControllerTest {
 //                .andExpect(model().attributeExists("searchTypes"))
         ;
         then(articleService).should().searchArticles(eq(null),eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(),anyInt());
+    }
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 검색어와 함께 호출")
+    @Test
+    public void givenSearchKeyword_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
+        //Given
+        SearchType searchType=SearchType.TITLE;
+        String searchValue="title";
+        given(articleService.searchArticles(eq(searchType),eq(searchValue), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(),anyInt())).willReturn(List.of(0,1,2,3,4));
+
+        //When&Then
+        mvc.perform(get("/articles")
+                        .queryParam("searchType", searchType.name())
+                        .queryParam("searchValue",searchValue)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+//                .andExpect(model().attributeExists("paginationBarNumbers"))
+                .andExpect(model().attributeExists("searchTypes"))
+        ;
+        then(articleService).should().searchArticles(eq(searchType),eq(searchValue), any(Pageable.class));
         then(paginationService).should().getPaginationBarNumbers(anyInt(),anyInt());
     }
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
